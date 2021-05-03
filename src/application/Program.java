1f5ecc5fdce6dcf1566b7987fe4ae1bca5f5bbdd
Alexsandro Morel <alexsandrofreitasmorel@gmail.com>
2021-05-03 11:34:53 -0300

    menos merda

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,18 +38,14 @@ public class Program
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 			
-			Date now = new Date();
-			if (checkIn.before(now) || checkOut.before(now))
+
+			String error = reservation.updateDates(checkIn, checkOut);
+			if(error != null) 
 			{
-				System.out.println("Reservation dates for update must be future dates");
+				System.out.println("Error in reservation" + error);
 			}
-			else if (!checkOut.after(checkIn))
-			{
-				System.out.println("Error in reservation: Check-out date must be after check-in date");
-			}
-			else
-			{
-				reservation.updateDates(checkIn, checkOut);
+			else 
+			{				
 				System.out.println("Reservation: " + reservation);
 			}
 									
